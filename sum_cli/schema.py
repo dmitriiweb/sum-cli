06c5ab711fd0e8798cdc5637cd2b97bc559b74d8
Dmitrii K <dmitriik@protonmail.com>
2025-03-27 21:46:55 +0700

    feat: add headers to web requests

--- sum_cli/schema.py
+++ sum_cli/schema.py
@@ -2,7 +2,14 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
+import requests
 from newspaper import Article
+
+REQUEST_DEFAULT_HEADERS = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Origin": "https://www.google.com",
+}
 
 
 @dataclass
@@ -12,7 +19,10 @@ class ArticleText:
 
     @classmethod
     def from_url(cls, url: str) -> ArticleText:
+        response = requests.get(url, headers=REQUEST_DEFAULT_HEADERS)
         article = Article(url)
+        article.set_html(response.text)
+        article.parse()
         try:
             article.download()
             article.parse()
